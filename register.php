da3f8316337c64bc19e725b7b9c6e731c19b71be
Zordon <65111609+Zordon1337@users.noreply.github.com>
2022-06-11 23:35:01 +0200

    another fixes
    register.php = Most bugged files on da world!???

--- register.php
+++ register.php
@@ -106,7 +106,7 @@
 					if ($polaczenie->query("INSERT INTO uzytkownicy VALUES (NULL, '$username', '$haslo_hash', '$email', 100, 100, 100, 14)"))
 					{
 						$_SESSION['udanarejestracja']=true;
-						header('Location: welcome.php');
+						header('Location: data.php');
 					}
 					else
 					{
